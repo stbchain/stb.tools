5e6cc2e4ab356805cbb354bbac3c6208f3f9ff95
Zou Jian <chsword@126.com>
2018-01-10 16:37:16 +0800

    split classes

--- src/STB.Tools.SQLToPOCO/ClassObject.cs
+++ src/STB.Tools.SQLToPOCO/ClassObject.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 
 namespace STB.Tools.SQLToPOCO
 {
@@ -8,54 +7,6 @@ namespace STB.Tools.SQLToPOCO
         public string Name { get; set; }
         public string Namespace { get; set; }
         public List<PropertyObject> Properties { get; set; } = new List<PropertyObject>();
-        public string Comment { get; set; }
-    }
-
-    public class PropertyObject
-    {
-        public string RawType { get; set; }
-
-        public string PropertyType
-        {
-            get
-            {
-                var type = RawType.Replace(",", "").Split("(").FirstOrDefault();
-                var pType = "";
-                switch (type)
-                {
-                    case "CHAR":
-                    case "VARCHAR":
-                    case "TEXT":
-                    case "LONGTEXT":
-                        return "string";
-                    case "timestamp":
-                    case "TIMESTAMP":
-                        pType = "DateTime";
-                        break;
-                    case "INT":
-                    case "INTEGER":
-                        pType = "int";
-                        break;
-                    case "BIGINT":
-                        pType = "long";
-                        break;
-                    case "TINYINT":
-                        pType = "byte";
-                        break;
-                }
-
-                if (string.IsNullOrWhiteSpace(pType))
-                    return $"error:{RawType}";
-                if (IsNotNull)
-                    pType += "?";
-                return pType;
-            }
-        }
-
-        public string PropertyName { get; set; }
-        public string DefaultValue { get; set; } = null;
-        public bool IsPrimary { get; set; }
-        public bool IsNotNull { get; set; }
         public string Comment { get; set; }
     }
 }
---
+++ src/STB.Tools.SQLToPOCO/PropertyObject.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+
+namespace STB.Tools.SQLToPOCO
+{
+    public class PropertyObject
+    {
+        public string RawType { get; set; }
+
+        public string PropertyType
+        {
+            get
+            {
+                var type = RawType.Replace(",", "").Split("(").FirstOrDefault();
+                var pType = "";
+                switch (type)
+                {
+                    case "CHAR":
+                    case "VARCHAR":
+                    case "TEXT":
+                    case "LONGTEXT":
+                        return "string";
+                    case "timestamp":
+                    case "TIMESTAMP":
+                        pType = "DateTime";
+                        break;
+                    case "INT":
+                    case "INTEGER":
+                        pType = "int";
+                        break;
+                    case "BIGINT":
+                        pType = "long";
+                        break;
+                    case "TINYINT":
+                        pType = "byte";
+                        break;
+                }
+
+                if (string.IsNullOrWhiteSpace(pType))
+                    return $"error:{RawType}";
+                if (IsNotNull)
+                    pType += "?";
+                return pType;
+            }
+        }
+
+        public string PropertyName { get; set; }
+        public string DefaultValue { get; set; } = null;
+        public bool IsPrimary { get; set; }
+        public bool IsNotNull { get; set; }
+        public string Comment { get; set; }
+    }
+}
